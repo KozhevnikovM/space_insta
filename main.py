6ddfde85094b68def37b30c66d7428b1af68e6d5
mk <mikhail.kozhevnikov@gmail.com>
2019-02-08 17:34:23 +0300

    Wrap image downloader in function

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests, os
 
-filename = 'images/hubble.jpg'
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-response = requests.get(url)
-
-with open(filename, 'wb') as file:
-    file.write(response.content)
 
 if not os.path.exists('images'):
     os.makedirs('images')
+
+def download_image(image_url, filename):
+    response = requests.get(image_url)
+    with open('images/{}'.format(filename), 'wb') as image_file:
+        image_file.write(response.content)
+
+
+if __name__ == '__main__':
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    filename = 'hubble.jpg'
+    download_image(url, filename)
