8948753b04d07f2d2371d5430f64b831579c07d9
mk <mikhail.kozhevnikov@gmail.com>
2019-02-08 17:27:48 +0300

    Added folder images creator

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import requests
+import requests, os
 
-filename = 'hubble.jpg'
+filename = 'images/hubble.jpg'
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 response = requests.get(url)
 
 with open(filename, 'wb') as file:
     file.write(response.content)
+
+if not os.path.exists('images'):
+    os.makedirs('images')
