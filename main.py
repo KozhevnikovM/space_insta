6427864c9f25995379b0dc6793c6ee6ee5753ace
mk <mikhail.kozhevnikov@gmail.com>
2019-02-08 17:21:59 +0300

    Simple image downloader

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+filename = 'hubble.jpg'
+url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+response = requests.get(url)
+
+with open(filename, 'wb') as file:
+    file.write(response.content)
