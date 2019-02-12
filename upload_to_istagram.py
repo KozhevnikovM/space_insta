fe7f969d1674d0bc17d5fefda516791a8cde25dd
mk <mikhail.kozhevnikov@gmail.com>
2019-02-12 13:14:13 +0300

    Wrap uploads in function

--- upload_to_istagram.py
+++ upload_to_istagram.py
@@ -13,16 +13,24 @@ def check_is_published(image_filename, log_file):
     with open(log_file, 'r') as file:
         return image_filename in file.read().splitlines()
 
+def upload_photos(username, password, photos_dir, file_with_published):
+    bot = instabot.Bot()
+    bot.login(username=username, password=password)
+    photos_list = os.listdir(photos_dir)
+
+    for photo in photos_dir:
+        if not check_is_published(photo, file_with_published):
+            bot.upload_photo('{}/{}'.format(photos_dir, photo))
+            add_to_published(photo, file_with_published)
+        return None
+
+
 
 if __name__ == '__main__':
     load_dotenv()
     bot = instabot.Bot()
-    bot.login(username=os.getenv('LOGIN'), password=os.getenv('PASSWORD'))
-    published = 'published.txt'
-    photos = os.listdir('images')
+    username, password = os.getenv('LOGIN'), os.getenv('PASSWORD')
+    published_file = 'published.txt'
+    photos_dir = 'images'
+    upload_photos(username, password, photos_dir, published_file)
 
-    for photo in photos:
-        if not check_is_published(photo, published):
-            bot.upload_photo('images/{}'.format(photo))
-            add_to_published(photo, published)
-        print('File already published')
