a281b35f54e85b40e7f331bf3f48b63e261f56bd
mk <mikhail.kozhevnikov@gmail.com>
2019-02-14 17:47:16 +0300

    Create folder with os.makedirs('images', exist_ok)

--- common.py
+++ common.py
@@ -6,8 +6,7 @@ def get_file_extension(file_url):
 
 
 def download_image(image_url, filename):
-    if not os.path.exists('images'):
-        os.makedirs('images')
+    os.makedirs('images', exist_ok=True)
     response = requests.get(image_url)
     if not response.ok:
         return None
